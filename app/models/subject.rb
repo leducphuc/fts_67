125190045e4f93244c353adcec16b36689ca4f37
ninh.duc.quyen <ninh.duc.quyen@gmail.com>
2016-09-16 12:40:46 +0700

    admin-create-subject

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -2,4 +2,7 @@ class Subject < ApplicationRecord
   has_many :exams, dependent: :destroy
   has_many :questions, dependent: :destroy
   has_many :suggest_questions, dependent: :destroy
+
+  validates :name, presence: true, uniqueness: true, length: {minimum: 6}
+  validates :duration, numericality: true
 end
